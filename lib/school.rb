8f3fe7bf4dc146f9ae477339cc2071bc447c041c
Alberto <ajsaenz1091@gmail.com>
2021-06-29 20:18:29 -0400

    Completed Assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= [] #if a is undefined then assign it a value of an empty string, else leave it as is. ||=
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.map do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+end
